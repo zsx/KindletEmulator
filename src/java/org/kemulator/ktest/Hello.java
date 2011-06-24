44afe60f853c2698a3b13d97051902ced437fd34
Shixin Zeng <zeng.shixin@gmail.com>
2011-06-23 22:16:13 -0500

    two versions (KButton/JButton) for comparison

--- src/java/org/kemulator/ktest/Hello.java
+++ src/java/org/kemulator/ktest/Hello.java
@@ -1,9 +1,16 @@
 package org.kemulator.ktest;
+
+import javax.swing.JButton;
+//import com.amazon.kindle.kindlet.ui.KButton;
+
 import com.amazon.kindle.kindlet.AbstractKindlet;
 import com.amazon.kindle.kindlet.KindletContext;
 
 public class Hello extends AbstractKindlet {
 	public void create(KindletContext ctx){
+		//KButton button = new KButton("hello");
+		JButton button = new JButton("hello");
+		ctx.getRootContainer().add(button);
 	}
 	public void start(){
 	}
