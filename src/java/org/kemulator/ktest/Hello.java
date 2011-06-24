60592ed3168d69dccaed3de0dadb8958bb43f546
Shixin Zeng <zeng.shixin@gmail.com>
2011-06-23 20:44:18 -0500

    add a test kindlet

---
+++ src/java/org/kemulator/ktest/Hello.java
@@ -0,0 +1,14 @@
+package org.kemulator.ktest;
+import com.amazon.kindle.kindlet.AbstractKindlet;
+import com.amazon.kindle.kindlet.KindletContext;
+
+public class Hello extends AbstractKindlet {
+	public void create(KindletContext ctx){
+	}
+	public void start(){
+	}
+	public void stop(){
+	}
+	public void destroy(){
+	}
+}
